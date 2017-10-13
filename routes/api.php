feed864b74eacc9976a2eafae98c6ff16ba9f81c
cheyycheyy <woshicheyy@sina.com>
2017-10-13 20:03:14 +0800

    增加跨域barryvdh/laravel-cors

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group([
-//    'middleware' => 'cors',
+    'middleware' => 'cors',
     'prefix' => 'v1',
 ], function() {
     // test
